693ba22df6d453c573182330e82b75b824fd86e0
Roro1005 <105412858+Roro1005@users.noreply.github.com>
2022-06-30 16:00:50 +0900

    Update Program.cs

--- class4/class4/Program.cs
+++ class4/class4/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using class4;
+
+//ハンズオン01
+/*
+long a = 10;
+long c = 5;
+
+long b =Functions.Power(a,c);
+
+Console.WriteLine(b);
+*/
+
+/*
+//ハンズオン02
+Console.WriteLine("体重を入力してください(kg)");
+double weight = Functions.WaitInputAndParseToDouble();
+Console.WriteLine("体重は" + weight.ToString("F2") + "です。");
+
+Console.WriteLine("身長を入力してください(m)");
+double height = Functions.WaitInputAndParseToDouble();
+Console.WriteLine("身長は" + height.ToString("F2") + "です。");
+
+double BMI = weight / (height * height);
+Console.WriteLine("BMIは" + BMI.ToString("F2") + "です。");
+*/
+for(int i = 0;i<args.Length;i++)
+{
+    Console.WriteLine("オプション"+i+":"+args[i]);
+}
+
+//ハンズオン04
+//argsに身長・体重が入っていたら入力受付をスキップする
+double weight=0;
+bool weightOptionIsValid=false;
+if(args.Length>0)
+{
+    string arg0=args[0];
+    weightOptionIsValid=double.TryParse(arg0,out weight);
+}
+if(!weightOptionIsValid)
+{
+    Console.WriteLine("体重を入力してください(kg)");
+    weight = Functions.WaitInputAndParseToDouble();
+}
+Console.WriteLine("体重は" + weight.ToString("F2") + "です。");
+
+double height = 0;
+bool heightOptionIsValid=false;
+if(args.Length > 1)
+{
+    string arg1=args[1];
+    heightOptionIsValid= double.TryParse(arg1, out height);
+}
+if(!heightOptionIsValid)
+{
+    Console.WriteLine("身長を入力してください(m)");
+    height = Functions.WaitInputAndParseToDouble();
+}
+Console.WriteLine("身長は" + height.ToString("F2") + "です。");
+
+double BMI = weight / (height * height);
+Console.WriteLine("BMIは" + BMI.ToString("F2") + "です。");

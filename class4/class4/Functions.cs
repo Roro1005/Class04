3880284c0d4c97844d0bc87380d1fd1bc33409e1
Roro1005 <105412858+Roro1005@users.noreply.github.com>
2022-07-14 14:40:36 +0900

    Update Functions.cs

--- class4/class4/Functions.cs
+++ class4/class4/Functions.cs
@@ -8,79 +8,49 @@ namespace class4
 {
     internal class Functions
     {
-        //ハンズオン01
-        
         public static int Square(int value)
         {
-            int result = value*value;
+            int result = value * value;
 
             return result;
         }
 
-        public static int Cube(int value)
-        {
-            int result = value*value*value;
-            return result;
-        }
-
-        public static int Sum(int value,int value2)
-        {
-            int result = value+value2;
-            return result;
-        }
-
-        public static int Sub(int value,int value2)
-        {
-            int result = value-value2;
-            return result;
-        }
-
-        public static int Division(int value,int value2)
-        {
-            int result = value/value2;
-            return result;
-        }
-
-        public static int Mul(int value,int value2)
-        {
-            int result=value*value2;
-            return result;
-        }
-
-       public static long Power(long value,long power)
+        //valueのn乗
+        public static long Power(long value, long power)
         {
             long result = 1;
-            for(int i=0;i<power;i++)
+            for (int i = 0; i < power; i++)
             {
-                result=result*value;
+                result = result * value;
             }
+
             return result;
         }
 
         //ハンズオン02の関数
-        //ユーザー入力を待機して、double型にバースして返す関数
-        //ユーザーがdouble型にバースできない文字列を入力したらリトライさせる
+        //ユーザー入力を待機して、double型にパースして返す関数
+        //ユーザーがdouble型にパースできない文字列を入力したらリトライさせる
         public static double WaitInputAndParseToDouble()
         {
-            double resust = 0;
+            double result = 0;
             bool parseSuccess = false;
             while (!parseSuccess)
             {
-                string userWeight = Console.ReadLine();
+                string userWeight = Console.ReadLine() ?? "";
+                parseSuccess = double.TryParse(userWeight, out result);
 
-                parseSuccess=double.TryParse(userWeight, out resust);
                 if (!parseSuccess)
-                {
-                    Console.WriteLine("数値を入力してください。");
-                }
+                    Console.WriteLine("数値を入力してください");
             }
-            return resust;
+
+            return result;
         }
 
-        //ハンズオン04
-        //string[] argsの中身を検知してバースを試みる
-        //バースに失敗した場合、ハンズオン02の関数を呼ぶ
-        public static double ParseArgsAndWaitInputIfParseFailed(string[] arguments,int argumetIndex,string askText)
+        //ハンズオン04の関数
+        //string[] argsの中身を検知してパースを試みる
+        //パースに失敗した場合、ハンズオン02の関数を呼ぶ
+        public static double ParseArgsAndWaitInputIfParseFailed
+            (string[] arguments, int argumetIndex, string askText)
         {
             double result = 0;
             bool weightOptionIsValid = false;
@@ -96,27 +66,35 @@ namespace class4
             }
             return result;
         }
-       //ハンズオン05の関数
-       //ノーマル
-       public static void NormalSample(int value)
+
+        //ハンズオン05の関数
+        //ノーマル
+        public static void NormalSample(int value)
         {
             value += 1;
         }
-
         public static void RefSample(ref int value)
         {
             value += 1;
         }
 
         //ハンズオン06
-        public static (double,double,double) GetvectorMagnitudeAndNormalized
+        public static (double, double, double) GetVectorMagnitudeAndNormalized
             (double x, double y)
         {
             //ルートをとる
-            double magnitude= Math.Sqrt(x*x+y*y);
+            double magnitude = Math.Sqrt(x * x + y * y);
 
             //正規化ベクトル
-            return(magnitude,x/magnitude,y/magnitude);
+            return (magnitude, x / magnitude, y / magnitude);
         }
+
+        //ハンズオン09の関数
+        public static uint GetRandomValue(uint seed)
+        {
+            return 1103515245 * seed + 12345;
+        }
+
     }
 }
+
